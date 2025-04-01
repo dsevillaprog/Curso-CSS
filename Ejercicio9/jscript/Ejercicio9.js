65cf0a3940fc3d565e91946eeba7f5a86091fbe5
DSevillaProg <dsevillaprog@proton.me>
2025-04-01 19:18:32 +0200

    Commit desde consola: Primer Commit

---
+++ Ejercicio9/jscript/Ejercicio9.js
@@ -0,0 +1,38 @@
+
+function vermas(id) {
+    if (id == "mas") {
+        document.getElementById("desplegar").style.display = "block";
+        document.getElementById("mas").style.display = "none";
+    }
+    else {
+        document.getElementById("desplegar").style.display = "none";
+        document.getElementById("mas").style.display = "inline";
+    }
+    if (id == "mas1") {
+        document.getElementById("desplegar1").style.display = "block";
+        document.getElementById("mas1").style.display = "none";
+    }
+    else {
+        document.getElementById("desplegar1").style.display = "none";
+        document.getElementById("mas1").style.display = "inline";
+    }
+    if (id == "mas-witcher") {
+        document.getElementById("desplegar-witcher").style.display = "block";
+        document.getElementById("mas-witcher").style.display = "none";
+    }
+    else {
+        document.getElementById("desplegar-witcher").style.display = "none";
+        document.getElementById("mas-witcher").style.display = "inline";
+    } witcher
+}
+// function deoferta() {
+//     if (class == "oferta") {
+//         document.getElementById('ver-ofertas').innerHTML = getElementsByClassName('oferta')
+//     }
+// }
+// function deoferta(class) {
+//     if (class == "oferta") {
+//         document.getElementsByClassName("oferta")
+//         document.getElementById('oferta').innerHTML = Date()"
+//     }
+// }
